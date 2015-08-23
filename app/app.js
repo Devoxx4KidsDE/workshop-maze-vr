7e9944eb066e9fdf41892c5dca3776202d256118
Benjamin <seber@synyx.de>
2015-08-23 18:48:55 +0200

    added alternative wall texture

--- app/app.js
+++ app/app.js
@@ -3,7 +3,11 @@ import wallsdata from './walls.js';
 import * as wall from './maze/wall.js';
 import * as maze from './maze/maze.js';
 
-const walls = wallsdata.map (w => wall.create (w));
+const walls = wallsdata.map ((w,i) => {
+    w = Object.assign (w);
+    w.texture = i % 2 ? 'wall' : 'wall_d4k';
+    return wall.create (w);
+});
 
 maze.init (walls);
 
