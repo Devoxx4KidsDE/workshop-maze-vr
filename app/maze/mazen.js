90ee38d45643f71a4934da734fb8d2e043e0e8cc
Tobias Schneider <schneider@synyx.de>
2015-08-28 17:36:53 +0200

    remove load from mazen
    * is not available in the mazeLoader

--- app/maze/mazen.js
+++ app/maze/mazen.js
@@ -182,27 +182,4 @@ function create({length, width, cellSize}) {
     return maze;
 }
 
-function load(name) {
-    return fetch('configuration/' + name + '.json').then(mazeConfigurationResponse => {
-
-        return mazeConfigurationResponse.json().then(mazeConfiguration => {
-            let maze = create({
-                length: mazeConfiguration.length,
-                width: mazeConfiguration.width,
-                cellSize: mazeConfiguration.cellSize
-            });
-
-            let walls = mazeConfiguration.walls.map((w, i) => {
-                w = Object.assign(w);
-                w.texture = i % 2 ? 'wall' : 'wall_d4k';
-                return wall.create(w, mazeConfiguration.cellSize);
-            });
-
-            maze.addWalls(walls);
-
-            return maze;
-        });
-    });
-}
-
-export {create, load}
+export {create}
