59233caaa79ed9984951a18496b3f7c7b4ca8429
Benjamin <seber@synyx.de>
2015-10-16 12:52:40 +0200

    reverted old wall texture because of flickering on mobilephone & cardboard

--- app/maze/wallTexture.js
+++ app/maze/wallTexture.js
@@ -1,6 +1,6 @@
 // string conforms the filename in /textures
 
-export const STONE = 'wall.jpg';
+export const STONE = 'wall.png';
 export const HEDGE = 'wall_hedge.jpg';
 export const LAMP  = 'wall_lamp.jpg';
 export const GATE  = 'wall_gate.jpg';
