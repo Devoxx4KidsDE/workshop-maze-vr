c890bc748e97c5eb625c5a82b161fddfa669c478
Benjamin Seber <seber@synyx.de>
2016-05-30 13:02:56 +0200

    maze html bootstrap cleanup

--- app/app.js
+++ app/app.js
@@ -25,29 +25,18 @@ function prepareHtml () {
 
     clearBody ();
 
-    const div = document.createElement.bind (document, 'div');
-
-    const controls = div();
-          controls.id = 'controls';
-
-    const player = div();
-          player.id = 'player-name';
-          player.classList.add ('player-information');
-
-    const items = div();
-          items.id = 'items';
-          items.classList.add ('items');
-          items.innerHTML = '<h1>Items</h1>';
-
-    const playground = div();
-          playground.id = 'maze';
-          playground.appendChild (controls);
-          playground.appendChild (player);
-          playground.appendChild (items);
-
-    const mazeContainer = div ();
-          mazeContainer.id = 'maze-container';
-          mazeContainer.appendChild (playground);
+    const mazeContainer = document.createElement ('div');
+    mazeContainer.innerHTML = `
+        <div id="maze-container">
+            <div id="maze">
+                <div id="controls"></div>
+                <div id="player-name" class="player-information"></div>
+                <div id="items" class="items">
+                    <h1>Items</h1>
+                </div>
+            </div>
+        </div>
+    `;
 
     const body = document.body;
           body.appendChild (mazeContainer);
