6cb202812c94a7fdc52638af23332f439a508e8f
Stefan Höhn <stefan@andreaundstefanhoehn.de>
2016-10-04 12:50:33 +0200

    more information on objects in the workshop and added special wall (image to be changed)

--- app/maze/wallTexture.js
+++ app/maze/wallTexture.js
@@ -4,3 +4,4 @@ export const STONE = 'wall.png';
 export const HEDGE = 'wall_hedge.jpg';
 export const LAMP  = 'wall_lamp.jpg';
 export const GATE  = 'wall_gate.jpg';
+export const SPECIAL  = 'wall_special.jpg';
